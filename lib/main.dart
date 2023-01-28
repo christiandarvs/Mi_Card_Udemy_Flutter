15fffa1209176a05f82617704c8cffe2a49a7520
christiandarvs <christiandarvin4@gmail.com>
2023-01-28 12:24:08 +0800

    add googlefonts, edit row to list tile, finish app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,14 +13,71 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
+          child: Center(
             child: Column(
-          children: const [
-            CircleAvatar(
-              radius: 60.0,
-              backgroundImage: AssetImage('images/IMG_20220908_150825_008.jpg'),
-            )
-          ],
-        )),
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage:
+                      AssetImage('images/IMG_20220908_150825_008.jpg'),
+                ),
+                Text(
+                  'Christian Darvin',
+                  style: GoogleFonts.pacifico(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40.0),
+                ),
+                Text(
+                  'Mobile App Developer'.toUpperCase(),
+                  style: GoogleFonts.sourceSansPro(
+                      color: Colors.grey.shade200,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0),
+                ),
+                const SizedBox(
+                  height: 40,
+                  width: 180,
+                  child: Divider(
+                    color: Colors.white,
+                    thickness: 2,
+                  ),
+                ),
+                Card(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 15.0),
+                    child: ListTile(
+                      leading: const Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '09123456789',
+                        style: GoogleFonts.sourceSansPro(
+                            color: Colors.teal.shade800, fontSize: 20),
+                      ),
+                    )),
+                Card(
+                  margin: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 15.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'myacc@gmail.com',
+                      style: GoogleFonts.sourceSansPro(
+                          color: Colors.teal.shade800, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
